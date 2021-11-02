ce78a94294f806c861a2632afd9951711ebd2d56
James Lambert <james.lambert@techcyte.com>
2021-11-02 16:33:34 -0600

    Begin work on level decor

--- src/ThemeWriter.h
+++ src/ThemeWriter.h
@@ -3,14 +3,24 @@
 
 #include <assimp/scene.h>
 #include <string>
+#include <map>
 #include "DisplayListSettings.h"
 
-class ThemeWriter {
+class ThemeMesh {
+public:
+    aiMesh* mesh;
+};
 
+class ThemeWriter {
 public:
     bool GetDecorName(const std::string& nodeName, std::string& output);
     void AppendContentFromScene(const aiScene* scene, DisplayListSettings& settings);
     std::string GetDecorID(const std::string & name);
+
+private:
+    void AppendContentFromNode(const aiNode* node, DisplayListSettings& settings);
+
+    std::map<std::string, ThemeMesh> mDecorMeshes;
 };
 
 #endif
