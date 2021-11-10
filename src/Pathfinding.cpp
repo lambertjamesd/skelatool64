c9eb58819c7e3777097b249a10c9ef89c047b435
James Lambert <lambertjamesd@gmail.com>
2021-11-09 19:41:57 -0700

    Remove duplicate pathing vertices

--- src/Pathfinding.cpp
+++ src/Pathfinding.cpp
@@ -1,6 +1,10 @@
 #include "Pathfinding.h"
 #include <math.h>
 
+bool shouldUsePathingPoint(float minY, const aiVector3D& point) {
+    return std::abs(minY - point.y) < 0.5f;
+}
+
 void buildPathingFromMesh(aiMesh* mesh, Pathfinding& result, const aiMatrix4x4& transform) {
     std::vector<aiVector3D> transformed;
 
@@ -16,9 +20,27 @@ void buildPathingFromMesh(aiMesh* mesh, Pathfinding& result, const aiMatrix4x4& 
     std::map<unsigned, unsigned> mIndexMapping;
 
     for (unsigned i = 0; i < mesh->mNumVertices; ++i) {
-        if (std::abs(minY - transformed[i].y < 0.5f)) {
-            mIndexMapping[i] = result.mPathingNodes.size();
-            result.mPathingNodes.push_back(transformed[i]);
+        if (shouldUsePathingPoint(minY, transformed[i])) {
+            bool didFindDuplicate = false;
+
+            for (unsigned dupeCheck = 0; dupeCheck < i; ++dupeCheck) {
+                if (!shouldUsePathingPoint(minY, transformed[dupeCheck])) {
+                    continue;
+                }
+
+                float dist = (transformed[i] - transformed[dupeCheck]).SquareLength();
+
+                if (dist < 0.001f) {
+                    mIndexMapping[i] = mIndexMapping[dupeCheck];
+                    didFindDuplicate = true;
+                    break;
+                }
+            }
+            
+            if (!didFindDuplicate) {
+                mIndexMapping[i] = result.mPathingNodes.size();
+                result.mPathingNodes.push_back(transformed[i]);
+            }
         }
     }
 
