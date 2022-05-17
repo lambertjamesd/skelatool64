2b60339f2be687a0080dd13525e8989e960e9c05
James Lambert <lambertjamesd@gmail.com>
2022-05-17 17:30:47 -0600

    Update setup script

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
-# git submodule update --init --remote yaml-cpp
 
-cd ../yaml-cpp
-cmake .
-make
+sudo apt install -y libpng-dev libtiff-dev libassimp-dev
+
+git clone https://github.com/jbeder/yaml-cpp.git
+
+cmake -S yaml-cpp -B yaml-cpp
+make -C yaml-cpp
+
+wget http://cimg.eu/files/CImg_latest.zip
+unzip CImg_latest.zip
+mv CImg-3.1.3_pre051622 cimg
